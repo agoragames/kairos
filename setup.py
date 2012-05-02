547c6e0eb2fc5cae86e9bcaa0730100322e11c92
Greg Banks <gbanks@mybasis.com>
2012-05-01 21:29:55 -0700

    fix setup.py (importing kairos before dependencies installed), move VERSION to __version__ (pep 396)

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
-import kairos
-import os
+import sys
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+requirements = [req.strip() for req in open('requirements.pip')]
+if sys.version_info[:2] <= (2, 6):
+    requirements = [req.strip() for req in open('requirements_py26.pip')]
 
-requirements = map(str.strip, open('requirements.pip').readlines())
+exec([v for v in open('kairos/__init__.py') if '__version__' in v][0])
 
 setup(
     name='kairos',
-    version=kairos.VERSION,
+    version=__version__,
     author='Aaron Westendorf',
     author_email="aaron@agoragames.com",
     packages = ['kairos'],
