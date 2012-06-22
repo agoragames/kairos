d463f93570ba4feacf576f523b60f785d5fcb872
Aaron Westendorf <aaron@agoragames.com>
2012-06-22 06:26:28 -0400

    Ignore the 'condensed' flag if resolution==step, fixes issue #3

--- kairos/timeseries.py
+++ kairos/timeseries.py
@@ -193,7 +193,7 @@ class Timeseries(object):
         rval[ resolution_buckets[idx]*resolution ] = data
     
     # If condensed, collapse the result into a single row
-    if condensed:
+    if condensed and step!=resolution:
       return reduce(operator.add, rval.values())
     return rval
 
@@ -247,7 +247,7 @@ class Timeseries(object):
           rval[ resolution_buckets[idx]*resolution ] = int(data) if data else 0
     
     # If condensed, collapse the result into a single sum
-    if condensed:
+    if condensed and step!=resolution:
       return sum(rval.values())
       
     return rval
@@ -338,7 +338,7 @@ class Timeseries(object):
           rval[interval_bucket*step][ resolution_buckets[x]*resolution ] = data
 
     # If condensed, collapse each interval into a single value
-    if condensed:
+    if condensed and step!=resolution:
       for key in rval.keys():
         if config.get('count_only',False):
           rval[key] = sum(rval[key].values())
