a63209e42def3e2e42a941c0854a4568805d4a93
Aaron Westendorf <aaron@agoragames.com>
2012-11-04 16:19:22 -0500

    Exceptions

---
+++ kairos/exceptions.py
@@ -0,0 +1,11 @@
+'''
+Copyright (c) 2012, Agora Games, LLC All rights reserved.
+
+https://github.com/agoragames/kairos/blob/master/LICENSE.txt
+'''
+
+class KairosException(Exception):
+  '''Base class for all kairos exceptions'''
+
+class UnknownInterval(KairosException):
+  '''The requested interval is not configured.'''
