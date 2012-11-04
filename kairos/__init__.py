b20e5bfa62993db1d1f32975ff951fde9dea87a1
Aaron Westendorf <aaron@agoragames.com>
2012-11-04 17:11:53 -0500

    Setting version to 0.1.0

--- kairos/__init__.py
+++ kairos/__init__.py
@@ -3,7 +3,7 @@ Copyright (c) 2012, Agora Games, LLC All rights reserved.
 
 https://github.com/agoragames/kairos/blob/master/LICENSE.txt
 '''
-__version__ = "0.0.7"
+__version__ = "0.1.0"
 
 from timeseries import Timeseries
 from exceptions import *
