76a3a639be4bd0fd17e0c4a8b93d0616b80703e6
Aaron Westendorf <aaron@agoragames.com>
2013-10-16 16:09:24 -0400

    SQL implementation of timeseries Upgrades to redis 2.8 Some fixes to work towards python3 compatibility

--- kairos/__init__.py
+++ kairos/__init__.py
@@ -3,7 +3,8 @@ Copyright (c) 2012-2013, Agora Games, LLC All rights reserved.
 
 https://github.com/agoragames/kairos/blob/master/LICENSE.txt
 '''
+from __future__ import absolute_import
 __version__ = "0.6.2"
 
-from timeseries import Timeseries
-from exceptions import *
+from .timeseries import Timeseries
+from .exceptions import *
