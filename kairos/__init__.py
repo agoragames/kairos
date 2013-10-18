816948d3a1bacfb9eb22d1178d6e72eb4cc3c1ea
Aaron Westendorf <aaron@agoragames.com>
2013-10-18 15:42:48 -0400

    kairos 0.6.3

--- kairos/__init__.py
+++ kairos/__init__.py
@@ -4,7 +4,7 @@ Copyright (c) 2012-2013, Agora Games, LLC All rights reserved.
 https://github.com/agoragames/kairos/blob/master/LICENSE.txt
 '''
 from __future__ import absolute_import
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 
 from .timeseries import Timeseries
 from .exceptions import *
