d9b5479e0291644fa82fa3b3b74de054b05701da
Aaron Westendorf <aaron@agoragames.com>
2013-10-21 13:06:00 -0400

    Adding SQL_HOST environment variable for testing multiple backends

--- test/functional/sql_timeseries_test.py
+++ test/functional/sql_timeseries_test.py
@@ -3,31 +3,33 @@ Functional tests for sql timeseries
 '''
 import time
 import datetime
+import os
 
 from sqlalchemy import create_engine
 
 from . import helpers
 
+SQL_HOST = os.environ.get('SQL_HOST', 'sqlite:///:memory:')
 class SqlSeriesTest(helpers.SeriesTest):
 
   def setUp(self):
-    self.client = create_engine('sqlite:///:memory:', echo=False)
+    self.client = create_engine(SQL_HOST, echo=False)
     super(SqlSeriesTest,self).setUp()
 
 class SqlHistogramTest(helpers.HistogramTest):
 
   def setUp(self):
-    self.client = create_engine('sqlite:///:memory:', echo=False)
+    self.client = create_engine(SQL_HOST, echo=False)
     super(SqlHistogramTest,self).setUp()
 
 class SqlCountTest(helpers.CountTest):
 
   def setUp(self):
-    self.client = create_engine('sqlite:///:memory:', echo=False)
+    self.client = create_engine(SQL_HOST, echo=False)
     super(SqlCountTest,self).setUp()
 
 class SqlGaugeTest(helpers.GaugeTest):
 
   def setUp(self):
-    self.client = create_engine('sqlite:///:memory:', echo=False)
+    self.client = create_engine(SQL_HOST, echo=False)
     super(SqlGaugeTest,self).setUp()
